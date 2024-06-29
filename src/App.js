4b61639be8da391a54afba61025dc27a2b4f8540
willian-matheus <85637800+williamtesche@users.noreply.github.com>
2024-06-28 23:23:39 -0300

    Ajustando o Front

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { getUsers, createUser } from "./Api";
-import { Input } from 'antd';
+import { Form, Input, Button, Row, Col } from "antd";
 
 const App = () => {
   const [users, setUsers] = useState([]);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [cpf, setCpf] = useState("");
+  const [form] = Form.useForm();
 
   const loadUsers = async () => {
     try {
@@ -17,53 +15,45 @@ const App = () => {
     }
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const newUser = { name, email ,cpf };
-    
+  const handleSubmit = async (values) => {
     try {
-      await createUser(newUser);
+      await createUser(values);
     } catch (error) {
       console.error("Error creating user:", error);
-    }finally{
+    } finally {
       loadUsers();
     }
   };
-  
+
   useEffect(() => {
     loadUsers();
   }, []);
 
   return (
     <div className="conterGlobal">
-      <form onSubmit={handleSubmit} className="conter">
-        <h1>Cadastro</h1>
-        <input
-          className="input"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Nome"
-        />
-        <br />
-        <input
-          className="input"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="E-mail"
-        />
-        <br />
-        <input
-          className="input"
-          type="text"
-          value={cpf}
-          onChange={(e) => setCpf(e.target.value)}
-          placeholder="CPF"
-        />
-        <br />
-        <button type="submit">Cadastrar</button>
-      </form>
+      <Form
+        form={form}
+        layout="vertical"
+        onFinish={handleSubmit}
+        scrollToFirstError={true}
+      >
+        <Row gutter={24}>
+          <Col xs={24} sm={12}>
+            <Form.Item name="name" label={"name"}>
+              <Input />
+            </Form.Item>
+            <Form.Item name="email" label={"email"}>
+              <Input />
+            </Form.Item>
+            <Form.Item name="cpf" label={"cpf"}>
+              <Input />
+            </Form.Item>
+            <Button type="primary" htmlType="submit" className="form-button">
+              enviar
+            </Button>
+          </Col>
+        </Row>
+      </Form>
       <ul>
         {users.map((user, index) => (
           <li key={index}>
